6c802ac01b6adfb4f42193c29f61dbee4462f37f
Francis Angoring <angoringfrancis29@gmail.com>
2024-05-01 12:25:38 +0800

    Added quiz room

---
+++ src/renderer/src/components/quiz_room/QuizRoom.tsx
@@ -0,0 +1,21 @@
+import { IoIosAdd } from 'react-icons/io'
+import { FaPlay } from "react-icons/fa6";
+
+export default function QuizRoom() {
+  return (
+    <div className="flex justify-center items-center h-screen gap-24">
+      <div className="text-myBlue-1 size-40 border-2 border-myBlue-1 rounded-md flex justify-center items-center text-center hover:cursor-pointer hover:bg-myBlue-2 hover:text-white ease-out duration-100">
+        <div>
+          <h2 className="font-bold text-xl">Create New Quiz</h2>
+          <IoIosAdd className="mx-auto" size={52} />
+        </div>
+      </div>
+      <div className="text-myBlue-1 size-40 border-2 border-myBlue-1 rounded-md flex justify-center items-center text-center hover:cursor-pointer hover:bg-myBlue-2 hover:text-white ease-out duration-100">
+        <div>
+          <h2 className="font-bold text-xl mb-2">Start Existing Quiz</h2>
+          <FaPlay className="mx-auto" size={32} />
+        </div>
+      </div>
+    </div>
+  )
+}
