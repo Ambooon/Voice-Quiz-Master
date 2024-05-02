8b6c728c8b80bf38bf7fb44dfe2fcc08b5c98cb5
Francis Angoring <angoringfrancis29@gmail.com>
2024-05-02 12:10:58 +0800

    Designed the Login & Register Page

--- src/renderer/src/components/authentication/Login.tsx
+++ src/renderer/src/components/authentication/Login.tsx
@@ -1,6 +1,5 @@
 import { SyntheticEvent, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
-import { UserDataType } from './Types'
 
 export default function Login() {
   const [userData, setUserData] = useState({
@@ -33,39 +32,43 @@ export default function Login() {
   // }
 
   return (
-    <section className="w-96 m-auto bg-neutral-50 p-4 text-center rounded-lg shadow-lg">
-      <h1 className="text-4xl font-bold mb-16 mt-4">Login</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          className="px-2 py-1 mb-6"
-          type="text"
-          name="username"
-          value={userData.username}
-          placeholder="Enter your username"
-          onChange={(e) => setUserData({ ...userData, username: e.target.value })}
-        />
-        <br />
-        <input
-          className="px-2 py-1 mb-8"
-          type="password"
-          name="password"
-          value={userData.password}
-          placeholder="Enter your password"
-          onChange={(e) => setUserData({ ...userData, password: e.target.value })}
-        />
-        <br />
-        {errorMessages &&
-          errorMessages.map((err) => (
-            <p key={err} className="text-red-500 font-light">
-              {err}
-            </p>
-          ))}
-        <button className="px-3 py-1 rounded-md bg-blue-400 mb-4 mt-2">Login</button>
-        <br />
-        <Link to="/register" className="font-thin text-sm">
-          Not yet registered?
-        </Link>
-      </form>
-    </section>
+    <div className="h-screen flex justify-center items-center">
+      <section className="w-96 m-auto bg-neutral-50 p-4 text-center rounded-lg shadow-xl">
+        <h1 className="text-4xl font-bold mb-12 mt-4">Login</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            className="px-2 py-1 mb-6 border border-slate-400 rounded-md"
+            type="text"
+            name="username"
+            value={userData.username}
+            placeholder="Enter your username"
+            onChange={(e) => setUserData({ ...userData, username: e.target.value })}
+          />
+          <br />
+          <input
+            className="px-2 py-1 mb-4 border border-slate-400 rounded-md"
+            type="password"
+            name="password"
+            value={userData.password}
+            placeholder="Enter your password"
+            onChange={(e) => setUserData({ ...userData, password: e.target.value })}
+          />
+          <br />
+          {errorMessages &&
+            errorMessages.map((err) => (
+              <p key={err} className="text-red-500 font-light">
+                {err}
+              </p>
+            ))}
+          <button className="px-3 py-1 rounded-md bg-myBlue-1 mb-8 mt-2 text-white hover:bg-myBlue-2">
+            Login
+          </button>
+          <br />
+          <Link to="/register" className="font-thin text-sm text-myBlue-1">
+            Not yet registered?
+          </Link>
+        </form>
+      </section>
+    </div>
   )
 }
--- src/renderer/src/components/authentication/Register.tsx
+++ src/renderer/src/components/authentication/Register.tsx
@@ -37,54 +37,58 @@ export default function Register() {
     return 0
   }
   return (
-    <section className="w-96 m-auto bg-neutral-50 p-4 text-center rounded-lg shadow-lg">
-      <h1 className="text-4xl font-bold mb-16 mt-4">Register</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          required
-          minLength={6}
-          className="px-2 py-1 mb-6"
-          type="text"
-          name="username"
-          value={userData.username}
-          placeholder="Enter your username"
-          onChange={(e) => setUserData({ ...userData, username: e.target.value })}
-        />
-        <br />
-        <input
-          required
-          minLength={8}
-          className="px-2 py-1 mb-6"
-          type="password"
-          name="password"
-          value={userData.password}
-          placeholder="Enter your password"
-          onChange={(e) => setUserData({ ...userData, password: e.target.value })}
-        />
-        <br />
-        <input
-          required
-          minLength={8}
-          className="px-2 py-1 mb-8"
-          type="password"
-          name="confirmPassword"
-          value={userData.confirmPassword}
-          placeholder="Confirm your password"
-          onChange={(e) => setUserData({ ...userData, confirmPassword: e.target.value })}
-        />
-        <br />
-        {errorMessages &&
-          errorMessages.map((err) => (
-            <p key={err} className="text-red-500 font-light">
-              {err}
-            </p>
-          ))}
-        <button className="px-3 py-1 rounded-md bg-blue-400 mb-4 mt-2">Register</button>
-        <br />
-        <Link to="/login" className="font-thin text-sm">
-          Already registered?
-        </Link>
-      </form>
-    </section>
+    <div className="h-screen flex justify-center items-center">
+      <section className="w-96 m-auto bg-neutral-50 p-4 text-center rounded-lg shadow-xl">
+        <h1 className="text-4xl font-bold mb-12 mt-4">Register</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            required
+            minLength={6}
+            className="px-2 py-1 mb-6 border border-slate-400 rounded-md"
+            type="text"
+            name="username"
+            value={userData.username}
+            placeholder="Enter your username"
+            onChange={(e) => setUserData({ ...userData, username: e.target.value })}
+          />
+          <br />
+          <input
+            required
+            minLength={8}
+            className="px-2 py-1 mb-6 border border-slate-400 rounded-md"
+            type="password"
+            name="password"
+            value={userData.password}
+            placeholder="Enter your password"
+            onChange={(e) => setUserData({ ...userData, password: e.target.value })}
+          />
+          <br />
+          <input
+            required
+            minLength={8}
+            className="px-2 py-1 mb-4 border border-slate-400 rounded-md"
+            type="password"
+            name="confirmPassword"
+            value={userData.confirmPassword}
+            placeholder="Confirm your password"
+            onChange={(e) => setUserData({ ...userData, confirmPassword: e.target.value })}
+          />
+          <br />
+          {errorMessages &&
+            errorMessages.map((err) => (
+              <p key={err} className="text-red-500 font-light">
+                {err}
+              </p>
+            ))}
+          <button className="px-3 py-1 rounded-md bg-myBlue-1 mb-8 mt-2 text-white hover:bg-myBlue-2">
+            Register
+          </button>
+          <br />
+          <Link to="/login" className="font-thin text-sm text-myBlue-1">
+            Already registered?
+          </Link>
+        </form>
+      </section>
+    </div>
   )
 }
